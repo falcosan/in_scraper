6292a3d755898e2acce146f55bd685da805e1ea9
Daniele Falchetti <danielefalche@gmail.com>
2025-07-15 01:17:30 +0200

    feat: Implement CompanyProfileSpider and PeopleProfileSpider for LinkedIn scraping

--- src/spiders/mod.rs
+++ src/spiders/mod.rs
@@ -1,9 +1,9 @@
 pub mod base;
 pub mod jobs;
-pub mod people_profile;
-pub mod company_profile;
+pub mod people;
+pub mod company;
 
 pub use jobs::JobsSpider;
 pub use base::{ Spider, Request };
-pub use people_profile::PeopleProfileSpider;
-pub use company_profile::CompanyProfileSpider;
+pub use people::PeopleProfileSpider;
+pub use company::CompanyProfileSpider;
